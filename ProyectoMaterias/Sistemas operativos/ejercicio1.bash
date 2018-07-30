bf71306454612d60d1496b7622eeaba6ecd01f13
Federico Pereira <fedepereirarey@gmail.com>
2018-07-30 16:39:06 -0300

    xd

--- ProyectoMaterias/Sistemas operativos/ejercicio1.bash
+++ ProyectoMaterias/Sistemas operativos/ejercicio1.bash
@@ -6,14 +6,14 @@ echo "Ingrese el usuario: "
 read usuario
 
 if [-n "$usuario"];
-then
-	last "$usuario" | grep "$usuario"
-	if [$! -eq 0];
 	then
-		echo "Se encontraron usuarios"
-	else
-		echo "No se encontraron regitros para este usuario en wtmp"
-	fi
+		last "$usuario" | grep "$usuario"
+		if [$! -eq 0];
+			then
+				echo "Se encontraron usuarios"
+		else
+			echo "No se encontraron regitros para este usuario en wtmp"
+		fi
 else
 	echo "No se ingreso ningun usuario. Se listaran todos los logins del sistema"
 	last -f /var/log/wtmp.1

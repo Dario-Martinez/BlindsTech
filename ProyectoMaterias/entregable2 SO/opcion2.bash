c0db8a5c5d0aa38dcc4abe3ace4292b727b115a0
Federico Pereira <fedepereirarey@gmail.com>
2018-09-28 09:22:16 -0300

    SO

---
+++ ProyectoMaterias/entregable2 SO/opcion2.bash
@@ -0,0 +1,8 @@
+function opcion2 {
+#Configurar directorio para respaldo
+#  • Se le pedirá al usuario la ruta (absoluta) del directorio al cual quiere respaldar.
+#  • Se le pedirá al usuario el nombre que le quiere poner al archivo de respaldo. 
+#  • Se le preguntará si quiere o no comprimir el directorio a respaldar. Si la respuesta es SI, entonces se deberá crear el respaldo comprimido, si la respuesta es NO entonces deberá empaquetar el directorio sin comprimir. (Los respaldos se deberán hacer en la carpeta /media/sf_shared_host_folder/respaldos)
+#  • Luego deberá listar la carpeta de respaldos.
+
+}
---
+++ ProyectoMaterias/entregable2 SO/opcion3.bash
@@ -0,0 +1,8 @@
+function opcion3 {
+    #Configurar CRON
+#  • Se le pedirá al usuario la hora, el día, el mes, el año y el día de la semana. (para cualquiera de los casos si el usuario no ingresa nada entonces significa que aplica para todos).
+#  • Se le pedirá al usuario el comando que quiere programar en la tarea. 
+#  • Se deberá crear el CRON para la determinada configuración que eligió el usuario con el comando que se pasó. 
+
+
+}

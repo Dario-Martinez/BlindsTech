2ad5542d50736b509c168e8d664dfd1c26a5517d
Darío <dario.martinez@anima.edu.uy>
2018-09-28 20:06:40 -0300

    Script sistemas menu, opcion 1 y 2

--- ProyectoMaterias/entregable2 SO/opcion1.bash
+++ ProyectoMaterias/entregable2 SO/opcion1.bash
@@ -1,39 +1,50 @@
-function opcion1 {
+#!/bin/bash
 
-#Configurar archivo para respaldo
-#  • Se le pedirá al usuario la ruta (absoluta) del archivo al cual quiere respaldar.
-#  • Se le pedirá al usuario el nombre que le quiere poner al archivo de respaldo. 
-
-
-
-    echo "Escriba la ruta absoluta de donde esta el archivo"
-    echo "ej: /home/federico/archivo.txt"
-    # -p rellena la opcion
-    read -p archabsoluta
-    if [ -f "$archabsoluta"]
+opcion1(){
+	#Configurar archivo para respaldo
+	#  • Se le pedirá al usuario la ruta (absoluta) del archivo al cual quiere respaldar.
+	#  • Se le pedirá al usuario el nombre que le quiere poner al archivo de respaldo. 
+	
+	echo "Escriba la ruta absoluta de donde esta el archivo"
+    	echo "ej: /home/federico/archivo.txt"
+    	
+	# -p rellena la opcion
+    	
+	read -p archabsoluta
+    	
+	if [ -f "$archabsoluta"];
         then
-        #  • Se le pedirá al usuario el nombre que le quiere poner al archivo de respaldo. 
-        echo "Escriba el nuevo nombre de el archivo"
-        read newname
-        #  • Se le preguntará si quiere o no comprimir el archivo a respaldar. Si la respuesta es SI, 
-        # entonces se deberá crear el respaldo comprimido, si la respuesta es NO entonces deberá 
-        # empaquetar el archivo sin comprimir. (Los respaldos se deberán hacer en la carpeta /media/sf_shared_host_folder/respaldos)
-        echo "Quiere comprimir el archivo? y/n"
-        read anw
-        if ["$anw" = "y"]
-            then
-            if [ -d /media/sf_shared_host_folder/respaldos/ ]
-            then
-                
-                tar -czf "/media/sf_shared_host_folder/respaldos/$newname" "$archabsoluta"
-                #  • Luego deberá listar la carpeta de respaldos.
-                ls "/media/sf_shared_host_folder/respaldos"
-            else
-                mkdir "/media/sf_shared_host_folder/respaldos/"
-            fi
-            
-            fi
-        fi
+        	#  • Se le pedirá al usuario el nombre que le quiere poner al archivo de respaldo. 
+        	echo "Escriba el nuevo nombre de el archivo"
+        	read newname
+        
+		#  • Se le preguntará si quiere o no comprimir el archivo a respaldar. Si la respuesta es SI, 
+        	# entonces se deberá crear el respaldo comprimido, si la respuesta es NO entonces deberá 
+        	# empaquetar el archivo sin comprimir. (Los respaldos se deberán hacer en la carpeta /media/sf_shared_host_folder/respaldos)
 
-
- }
+		echo "Quiere comprimir el archivo? y/n"
+        	read anw
+        	
+		if ["$anw" = "y"];
+            	then
+            		if [ -d /media/sf_shared_host_folder/respaldos/ ];
+            		then
+                	tar -czf "/media/sf_shared_host_folder/respaldos/$newname" "$archabsoluta"
+                	
+			#  • Luego deberá listar la carpeta de respaldos.
+                	
+			ls "/media/sf_shared_host_folder/respaldos"
+            		else
+				mkdir "/media/sf_shared_host_folder/respaldos/"
+            		fi
+		else
+			if [ /d /media/sf_shared_host_folder/respaldos/ ];
+			then
+				tar -cf "/media/sf_shared_host_folder/respaldos/$newname" "$archabsoluta"
+				ls "/media/sf_shared_host_folder/respaldos/"
+			else
+				mkdir "/media/sf_shared_host_folder/respaldos/"
+			fi
+		fi
+	fi
+}
--- ProyectoMaterias/entregable2 SO/opcion2.bash
+++ ProyectoMaterias/entregable2 SO/opcion2.bash
@@ -1,8 +1,50 @@
-function opcion2 {
-#Configurar directorio para respaldo
-#  • Se le pedirá al usuario la ruta (absoluta) del directorio al cual quiere respaldar.
-#  • Se le pedirá al usuario el nombre que le quiere poner al archivo de respaldo. 
-#  • Se le preguntará si quiere o no comprimir el directorio a respaldar. Si la respuesta es SI, entonces se deberá crear el respaldo comprimido, si la respuesta es NO entonces deberá empaquetar el directorio sin comprimir. (Los respaldos se deberán hacer en la carpeta /media/sf_shared_host_folder/respaldos)
-#  • Luego deberá listar la carpeta de respaldos.
+﻿#!/bin/bash
 
+opcion2(){
+	#Configurar archivo para respaldo
+	#  • Se le pedirá al usuario la ruta (absoluta) del archivo al cual quiere respaldar.
+	#  • Se le pedirá al usuario el nombre que le quiere poner al archivo de respaldo. 
+	
+	echo "Escriba la ruta absoluta en donde esta el directorio"
+    	echo "ej: /home/alumno"
+    	
+	# -p rellena la opcion
+    	
+	read -p rutabsoluta
+    	
+	if [ -d "$rutabsoluta"];
+        then
+        	#  • Se le pedirá al usuario el nombre que le quiere poner al archivo de respaldo. 
+        	echo "Escriba el nuevo nombre del directorio"
+        	read newname
+        
+		#  • Se le preguntará si quiere o no comprimir el archivo a respaldar. Si la respuesta es SI, 
+        	# entonces se deberá crear el respaldo comprimido, si la respuesta es NO entonces deberá 
+        	# empaquetar el archivo sin comprimir. (Los respaldos se deberán hacer en la carpeta /media/sf_shared_host_folder/respaldos)
+
+		echo "Quiere comprimir el directorio? y/n"
+        	read anw
+        	
+		if ["$anw" = "y"];
+            	then
+            		if [ -d /media/sf_shared_host_folder/respaldos/ ];
+            		then
+                	tar -czf "/media/sf_shared_host_folder/respaldos/$newname" "$archabsoluta"
+                	
+			#  • Luego deberá listar la carpeta de respaldos.
+                	
+			ls "/media/sf_shared_host_folder/respaldos"
+            		else
+				mkdir "/media/sf_shared_host_folder/respaldos/"
+            		fi
+		else
+			if [ /d /media/sf_shared_host_folder/respaldos/ ];
+			then
+				tar -cf "/media/sf_shared_host_folder/respaldos/$newname" "$archabsoluta"
+				ls "/media/sf_shared_host_folder/respaldos/"
+			else
+				mkdir "/media/sf_shared_host_folder/respaldos/"
+			fi
+		fi
+	fi
 }

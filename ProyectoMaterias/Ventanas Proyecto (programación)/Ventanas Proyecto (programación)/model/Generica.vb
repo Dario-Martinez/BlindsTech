cb92650f549ad21885d6c75d8ebd139714cd2cf1
Federico Pereira <fedepereirarey@gmail.com>
2018-10-03 17:53:20 -0300

    skere

--- ProyectoMaterias/Ventanas Proyecto (programación)/Ventanas Proyecto (programación)/model/Generica.vb
+++ ProyectoMaterias/Ventanas Proyecto (programación)/Ventanas Proyecto (programación)/model/Generica.vb
@@ -6,15 +6,28 @@ Public Class Generica
     ' Protected: Acceso desde clase y herencia
     ' Private: Acceso solo desde la clase
     Protected nomTabla As String
-
+    Protected atributosInsert() As String
 
     Public Overridable Function obtenerListado() As SqlDataReader
         Return DBConn.Instance.SelectStatement("SELECT * FROM " + nomTabla)
     End Function
 
-    Protected atributosInsert() As String
 
     Function insertar() As Integer
+        Dim insert As String = "INSERT INTO " & nomTabla & "( " & String.Join(",", atributosInsert) & ")_
+        VALUES (@" & String.Join("@,", atributosInsert) & " ) "
+        Dim comando As New SqlCommand(insert)
+        For Each atributo In atributosInsert
+            comando.Parameters.AddWithValue("@" & atributo, CallType.Get)
+        Next
 
+        'If (insert.ExecuteNonQuery().Equals(1)) Then
+        'MsgBox("ya esta en la db")
+        'Else
+        '    MsgBox("No esta que lo inserte")
+
+        'End If
+
+        Return True
     End Function
 End Class

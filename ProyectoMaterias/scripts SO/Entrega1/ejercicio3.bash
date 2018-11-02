ef51300a8f31311297e13da4d8afdb0783d6321c
dario Martinez <dario.martinez@anima.edu.uy>
2018-11-02 18:28:47 -0300

    so y bd

--- ProyectoMaterias/scripts SO/Entrega1/ejercicio3.bash
+++ ProyectoMaterias/scripts SO/Entrega1/ejercicio3.bash
@@ -5,7 +5,11 @@ if [ -f "/media/sf_shared_host_folder/respaldos" ];
 then
 	fecha= $(date "+%Y/%m/%d-%H:%m")
 	ruta_home="/media/sf_shared_host_folder/respaldos/respaldo_home_$fecha.tar.gz"
+	ruta_etc=""
+	ruta_var=""
 	tar -czvf "$ruta_home" "/home"
+	tar -czvf "$ruta_etc" "/etc"
+	tar -czvf "$ruta_var" "/var"
 	echo "El usuario: $user a hecho un respaldo en: $ruta_home." >> /var/log/mis_respaldos
 fi
 
